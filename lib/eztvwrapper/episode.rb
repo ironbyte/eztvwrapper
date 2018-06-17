a5f4c9ff24257e4cafaf3be5e54b96e07911bf99
Moncy <illusivespectre@gmail.com>
2018-06-17 17:56:28 +0200

    Implement Episode class and update episode_spec.rb

--- lib/eztvwrapper/episode.rb
+++ lib/eztvwrapper/episode.rb
@@ -1,4 +1,20 @@
 module EZTVWrapper
   class Episode
+    attr_reader :basename, :show, :season, :number, :links
+
+    def initialize(episode_hash)
+      @basename = episode_hash.fetch(:episode_basename)
+      @links = episode_hash.fetch(:episode_links)
+
+      @show = episode_info.fetch("show_name")
+      @season = episode_info.fetch("season_no").to_i
+      @number = episode_info.fetch("episode_no").to_i
+    end
+
+    private
+
+    def episode_info
+      @episode_info ||= EZTVWrapper::REGEX_SCENE_TV.match(@basename).named_captures
+    end
   end
 end
--- spec/episode_spec.rb
+++ spec/episode_spec.rb
@@ -1,5 +1,4 @@
 RSpec.describe EZTVWrapper::Episode do
-  subject { described_class }
   let(:episode_hash) {
     {
       episode_basename: "Mr.Robot.S02E06.PROPER.720p.HDTV.x264-KILLERS[eztv].mkv",
@@ -10,27 +9,45 @@ RSpec.describe EZTVWrapper::Episode do
              }
     }
   }
+  subject(:valid_episode) { EZTVWrapper::Episode.new(episode_hash) }
 
   describe "#initialize(episode_hash)" do
-    it "returns a valid instance of Episode class"
+    it "returns a valid instance of Episode class" do
+      expect{ valid_episode }.not_to raise_error
+      expect(valid_episode).to be_instance_of EZTVWrapper::Episode
+    end
   end
 
   describe "#basename" do
-    it "returns the original episode name as a string"
+    it "returns the original episode name as a string" do
+      expect(valid_episode.basename).to eql "Mr.Robot.S02E06.PROPER.720p.HDTV.x264-KILLERS[eztv].mkv"
+    end
   end
 
   describe "#show" do
-    it "returns the correct name of the TV Show as a string"
+    it "returns the correct name of the TV Show as a string" do
+      expect(valid_episode.show).to eql "Mr.Robot"
+    end
   end
 
   describe "#season" do
-    it "returns the correct season number as an integer"
+    it "returns the correct season number as an integer" do
+      expect(valid_episode.season).to eql 2
+    end
   end
 
   describe "#number" do
-    it "returns the correct episode number as an integer"
+    it "returns the correct episode number as an integer" do
+      expect(valid_episode.number).to eql 6
+    end
   end
 
   describe "#links" do
+    it "returns a collection of valid links of an episode" do
+      expect(valid_episode.links).to eql episode_hash.fetch(:episode_links)
+      expect(valid_episode.links.fetch(:episode_url)).to eql "/ep/153624/mr-robot-s02e06-proper-720p-hdtv-x264-killers/"
+      expect(valid_episode.links.fetch(:torrent_url)).to eql "https://zoink.ch/torrent/Mr.Robot.S02E06.PROPER.720p.HDTV.x264-KILLERS[eztv].mkv.torrent"
+      expect(valid_episode.links.fetch(:magnet_uri)).to eql "magnet:?xt=urn:btih:4de55d9171f25362860ed72b0e9c678b087dcaae&dn=Mr.Robot.S02E06.PROPER.720p.HDTV.x264-KILLERS%5Beztv%5D.mkv%5Beztv%5D&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A80&tr=udp%3A%2F%2Fglotorrents.pw%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337%2Fannounce&tr=udp%3A%2F%2Fexodus.desync.com%3A6969"
+    end
   end
 end
